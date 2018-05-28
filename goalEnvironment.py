588b1e389a7b86651a8648a4d03f566e2e84cdf9
harjatinsingh <harjatin@gmail.com>
2018-05-28 17:47:57 -0400

    Step function added ro RL environment

--- goalEnvironment.py
+++ goalEnvironment.py
@@ -8,7 +8,7 @@ from os import path
 
 class goalEnvironment():
 
-	 def __init__(self, model_path, distance_threshold = 1e-1):
+	 def __init__(self, model_path, distance_threshold = 1e-1, frame_skip = 1):
 
 	 	if model_path.startswith("/"):
 	 		fullpath = model_path
@@ -22,6 +22,7 @@ class goalEnvironment():
 	 	self.data = self.sim.data
 	 	self.viewer = MjViewer(self.sim)
 	 	self.distance_threshold = distance_threshold
+	 	self.frame_skip = frame_skip
 
 	 def viewer_setup(self):
 
@@ -42,11 +43,17 @@ class goalEnvironment():
 	 	self.sim.data.qpos[2] = xLoc
 	 	self.sim.data.qpos[3] = yLoc	
 
-	 def set_distance_threshold(distance_threshold):
+	 def set_distance_threshold(self,distance_threshold):
 	 	self.distance_threshold = distance_threshold
 
-	 def get_distance_threshold(distance_threshold):
-	 	return(self.distance_threshold) 
+	 def set_frame_skip(self,frame_skip):
+	 	self.frame_skip = frame_skip
+
+	 def get_frame_skip(self):
+	 	return (self.frame_skip)
+
+	 def get_distance_threshold(self):
+	 	return (self.distance_threshold) 
 
 	 def get_ball_location(self):
 	 	return (self.sim.data.qpos[2:4])	
@@ -62,6 +69,9 @@ class goalEnvironment():
 	 	self.sim.data.ctrl[0] = xDirectionControl
 	 	self.sim.data.ctrl[1] = yDirectionControl
 
+	 def get_current_observation(self):
+	 	return np.concatenate([self.get_goal_location(), self.get_ball_location(), self.get_ball_velocity()]).ravel()
+
 	 def get_image_of_goal_observation(self, xLoc = None, yLoc = None):
 	 	
 	 	if not xLoc:
@@ -76,6 +86,21 @@ class goalEnvironment():
 	 	self.sim.data.qpos[3] = yLoc
 
 	 	self.render_view()
+
+	 def do_simulation(self, ctrl, n_frames):
+
+	 	self.send_control_command(ctrl[0],ctrl[1])
+	 	for _ in range(n_frames):
+	 		self.take_step()
+	
+
+	 def step(self, ctrl):
+	 	
+	 	self.do_simulation(ctrl, self.frame_skip)
+	 	obs = self.get_current_observation()
+	 	reward = self.get_reward()
+	 	done = (reward == 1.0)
+	 	return obs, reward, done
 
 	 def take_step(self):
 	 	self.sim.step()
--- simulation.py
+++ simulation.py
@@ -9,9 +9,10 @@ from goalEnvironment import goalEnvironment
 env = goalEnvironment("/home/frc-vision/Desktop/git/mujoco-env/xmls/goal.xml")
 env.viewer_setup()
 env.set_goal_location(0.3,0.3)
+env.set_frame_skip(2)
 
 while True:
-	env.send_control_command(0.002,0.002)
-	env.take_step()
+	#env.send_control_command(0.002,0.002)
+	ctrl = np.asarray([0.002,0.002])
+	obs, reward, done = env.step(ctrl)
 	env.render_view()
-	
